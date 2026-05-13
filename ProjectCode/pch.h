5f52f93d1b56dd9d573ff9e7e05a7d97756879d3
Ahmed Hossam <ahmed.hos2974@gmail.com>
2026-05-13 21:59:36 +0300

    Add files via upload

---
+++ ProjectCode/pch.h
@@ -0,0 +1,22 @@
+// pch.h: This is a precompiled header file.
+// Files listed below are compiled only once, improving build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, files listed here are ALL re-compiled if any one of them is updated between builds.
+// Do not add frequently updated files here, as this negates the performance advantage.
+
+#ifndef PCH_H
+#define PCH_H
+
+#pragma once
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <fstream>
+#include <sstream>
+#include <cstdio>
+#include <algorithm>
+
+#include "gtest/gtest.h"
+#include "HospitalService.h"
+#include "HospitalRepository.h" 
+#endif //PCH_H
